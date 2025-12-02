431ca8cce43705e3e5eaaac883019ac3e5d5ba7e
swngmls3571-alt <swngmls3571@naver.com>
2025-12-02 17:40:02 +0900

    주ㅠ희

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -24,6 +24,7 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+          테스트
         </p>
       </div>
       <p className="read-the-docs">
