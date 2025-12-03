5c9cf392a8cf7d3a811f47826b5ed32d7fa10f37
swngmls3571-alt <swngmls3571@naver.com>
2025-12-03 09:37:59 +0900

    다시

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -15,7 +15,6 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <p>ㅗㅗddddddd</p>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -24,8 +23,7 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-          2025.12.03
-        </p>
+        </p>ㄹㄹ
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
